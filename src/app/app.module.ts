006ae7aad599339fbc74840f4a0e0c139a1d0122
Quang Vinh <qvinhdx@gmail.com>
2022-10-06 17:40:11 +0700

    chuanbi merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,20 @@ import { ShopComponent } from './pages/shop/shop.component';
 import { SearchPipe } from './pipes/search.pipe';
 import { CompareComponent } from './pages/compare/compare.component';
 import { PcBuildingComponent } from './pages/pc-building/pc-building.component';
-// import { AboutComponent } from './pages/about/about.component';
-// import { WallpaperComponent } from './pages/wallpaper/wallpaper.component';
-// import { AboutgComponent } from './pages/aboutg/aboutg.component';
-// import { WarrantyComponent } from './pages/warranty/warranty.component';
-// import { ContactComponent } from './pages/contact/contact.component';
-// import { NewComponent } from './pages/new/new.component';
-// import { BlogComponent } from './pages/blog/blog.component';
-// import { DetailblogComponent } from './pages/detailblog/detailblog.component';
-// import { DetailnewComponent } from './pages/detailnew/detailnew.component';
-// import { HomeComponent } from './pages/home/home.component';
-// import { CarouselModule } from 'ngx-owl-carousel-o';
-// import { UserIComponent } from './pages/user-i/user-i.component';
-// import { UserSettingComponent } from './pages/user-setting/user-setting.component';
-// import { PasswordComponent } from './pages/password/password.component';
+import { AboutComponent } from './pages/about/about.component';
+import { WallpaperComponent } from './pages/wallpaper/wallpaper.component';
+import { AboutgComponent } from './pages/aboutg/aboutg.component';
+import { WarrantyComponent } from './pages/warranty/warranty.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NewComponent } from './pages/new/new.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { DetailblogComponent } from './pages/detailblog/detailblog.component';
+import { DetailnewComponent } from './pages/detailnew/detailnew.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { UserIComponent } from './pages/user-i/user-i.component';
+import { UserSettingComponent } from './pages/user-setting/user-setting.component';
+import { PasswordComponent } from './pages/password/password.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,19 +33,19 @@ import { PcBuildingComponent } from './pages/pc-building/pc-building.component';
     SearchPipe,
     CompareComponent,
     PcBuildingComponent,
-    // AboutComponent,
-    // WallpaperComponent,
-    // AboutgComponent,
-    // WarrantyComponent,
-    // ContactComponent,
-    // NewComponent,
-    // BlogComponent,
-    // DetailblogComponent,
-    // DetailnewComponent,
-    // HomeComponent,
-    // UserIComponent,
-    // UserSettingComponent,
-    // PasswordComponent,
+    AboutComponent,
+    WallpaperComponent,
+    AboutgComponent,
+    WarrantyComponent,
+    ContactComponent,
+    NewComponent,
+    BlogComponent,
+    DetailblogComponent,
+    DetailnewComponent,
+    HomeComponent,
+    UserIComponent,
+    UserSettingComponent,
+    PasswordComponent,
   ],
   imports: [
     BrowserModule,
